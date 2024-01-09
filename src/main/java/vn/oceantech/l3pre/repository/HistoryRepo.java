4d2ba0ac7cf943117af123a6c8378061aec133b5
longlh <lehoanglongncth@gmail.com>
2024-01-09 14:55:03 +0700

    send remedy

--- src/main/java/vn/oceantech/l3pre/repository/HistoryRepo.java
+++
@@ -1,6 +0,0 @@
-package vn.oceantech.l3pre.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HistoryRepo extends JpaRepository<History, Integer> {
-}
--- src/main/java/vn/oceantech/l3pre/service/impl/HistoryExtraServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package vn.oceantech.l3pre.service.impl;
-
-import org.springframework.stereotype.Service;
-import vn.oceantech.l3pre.service.HistoryExtraService;
-
-@Service
-public class HistoryExtraServiceImpl implements HistoryExtraService {
-}
